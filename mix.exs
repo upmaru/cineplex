c073e6dc994f9bbea7ca93da3e5ec9dfdb91bdf3
Zack Siri <zack@codemy.net>
2017-12-26 19:13:54 +0700

    update deps

--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule Compressor.Mixfile do
       {:ffmpex, "~> 0.4.1"},
       {:honeydew, "~> 1.0.4"},
       {:download, "~> 0.0.4"},
-      {:blazay, "~> 1.2.0"}
+      {:blazay, "~> 1.2.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
