b9527fb7f6547e5c1dfa1fba3144e9166e4e88e6
Zack Siri <zack@codemy.net>
2018-02-14 20:45:24 +0700

    fix configuration

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,12 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :exq,
+  name: Exq,
+  namespace: "exq",
+  concurrency: 1,
+  queues: ["encoder"],
+  start_on_application: false
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
--- config/prod.exs
+++ config/prod.exs
@@ -1,9 +1,4 @@
 use Mix.Config
 
 config :exq,
-  name: Exq,
   url: "${REDIS_URL}"
-  namespace: "exq",
-  concurrency: 1,
-  queues: ["encoder"],
-  start_on_application: false
