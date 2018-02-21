13fd7c862f4ad43a8a003c1c76acd91ab0c8c39a
Zack Siri <zack@codemy.net>
2018-02-21 18:29:16 +0700

    update hex

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Compressor.Mixfile do
   def project do
     [
       app: :compressor,
-      version: "0.3.5",
+      version: "0.4.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       source_url: "https://github.com/upmaru/compressor",
@@ -37,7 +37,7 @@ defmodule Compressor.Mixfile do
       {:exq, "~> 0.10.1"},
       {:download, github: "little-bobby-tables/download", branch: "fix-process-communication"},
       {:httpoison, "~> 1.0"},
-      {:upstream, path: "../upstream"},
+      {:upstream, "~> 1.3.4"},
       {:pid_file, "~> 0.1.1", only: [:prod]},
       {:distillery, "~> 1.5", runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev}
