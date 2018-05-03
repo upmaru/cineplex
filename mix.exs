494af6ad61636387a1e487cbc6c602dcef4209fa
Zack Siri <zack@codemy.net>
2018-05-03 17:58:47 +0700

    update version for compressor

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Compressor.Mixfile do
   def project do
     [
       app: :compressor,
-      version: "0.4.12",
+      version: "0.5.0",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
