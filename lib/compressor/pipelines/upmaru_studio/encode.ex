92acd7eb801440cbd53e7afe13a2366ce3e02947
Zack Siri <zack@artellectual.com>
2018-11-18 10:40:40 +0700

    prefix pipelines

--- lib/compressor/pipelines/upmaru_studio/encode.ex
+++ lib/compressor/pipelines/upmaru_studio/encode.ex
@@ -1,11 +1,11 @@
-defmodule Pipelines.UpmaruStudio.Encode do
+defmodule Compressor.Pipelines.UpmaruStudio.Encode do
   alias Compressor.Queue.Job
-  alias Pipelines.UpmaruStudio.Encode
+  alias Compressor.Pipelines.UpmaruStudio.Encode
 
   def perform(%Job.Entry{job: job} = job_entry) do
     with {:ok, url, path} <- Encode.Setup.perform(job),
          {:ok, downloaded} <- Encode.Download.perform(url, path),
-         {:ok, transcoded} <- Encode.Transcode.perform(job)
+         {:ok, transcoded} <- Encode.Transcode.perform(job_entry)
     do
 
     else
