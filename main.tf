091eb8f2bdd9e35692fbea821a4721ea43fd0e0a
Zack Siri <zacksiri@gmail.com>
2018-11-22 12:55:37 +0700

    Fix terraform script

--- main.tf
+++ main.tf
@@ -59,8 +59,8 @@ resource "null_resource" "cineplex_server_updater" {
   provisioner "remote-exec" {
     inline     = [
       "gcsfuse -o ro --implicit-dirs packages.apk.build /mnt/packages",
-      "rm -f /var/lib/studio/.self",
-      "echo ${lxd_container.cineplex_server.*.ip_address[count.index]} > /var/lib/studio/.self",
+      "rm -f /var/lib/cineplex/.self",
+      "echo ${lxd_container.cineplex_server.*.ip_address[count.index]} > /var/lib/cineplex/.self",
       "apk update && apk add --upgrade cineplex@upmaru",
       "fusermount -u /mnt/packages"
     ]
