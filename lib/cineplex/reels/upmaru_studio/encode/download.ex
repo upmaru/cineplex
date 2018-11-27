dd80ce18477546c8a2c56174ff5cd352ab02db58
Zack Siri <zacksiri@gmail.com>
2018-11-27 18:51:33 +0700

    Added on_fail hook for download

--- lib/cineplex/reels/upmaru_studio/encode/download.ex
+++ lib/cineplex/reels/upmaru_studio/encode/download.ex
@@ -1,11 +1,21 @@
 defmodule Cineplex.Reels.UpmaruStudio.Encode.Download do
-  @spec perform(binary(), binary()) :: {:ok, binary()} | {:error, :download_failed}
-  def perform(url, path) do
+  require Logger
+
+  @spec perform(binary(), binary(), [{:on_fail, fun}]) :: {:ok, binary()} | {:error, :download_failed}
+  def perform(url, path, opts \\ []) do
     with {:ok, file} <- File.open(path, [:write]),
          {:ok, _device} <- Downstream.get(url, file, timeout: :infinity) do
       {:ok, path}
     else
-      _ -> {:error, :download_failed}
+      _ ->
+        on_fail(opts)
+        {:error, :download_failed}
     end
+  end
+
+  defp on_fail(opts) do
+    Keyword.get(opts, :on_fail, fn ->
+      Logger.error("[Cineplex.Reels.UpmaruStudio.Encode.Download] failed")
+    end)
   end
 end
