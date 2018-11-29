02067a43083f3461c7d971cf33ff1f1ff6ece1ea
Zack Siri <zack@artellectual.com>
2018-11-29 14:55:55 +0700

    update version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Compressor.Mixfile do
   def project do
     [
       app: :cineplex,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
