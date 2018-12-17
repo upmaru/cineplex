d48c783c43a3b8fa63cf5e782891cffd8d785093
Zack Siri <zack@artellectual.com>
2018-12-17 14:27:04 +0700

    only log warn

--- config/test.exs
+++ config/test.exs
@@ -6,6 +6,8 @@ config :cineplex, :role, System.get_env("CINEPLEX_ROLE")
 
 config :tesla, adapter: Tesla.Adapter.Hackney
 
+config :logger, level: :warn
+
 config :cineplex, Cineplex.Repo,
   database: "cineplex_test",
   username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || "zacksiri",
