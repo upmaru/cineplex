1cd291f7d5f67c8ebc63161b398eebc2775c9587
Zack Siri <zack@artellectual.com>
2018-12-17 14:25:28 +0700

    remove job callback as a requirement

--- lib/cineplex/reel.ex
+++ lib/cineplex/reel.ex
@@ -6,10 +6,6 @@ defmodule Cineplex.Reel do
   @callback setting(Tesla.Client.t()) ::
               {:ok, %{presets: list(), storage: map()}} | {:error, :invalid_setting}
 
-  @callback job(Tesla.Client.t()) ::
-              {:ok, %{object: binary(), resource: binary(), events_callback_url: binary()}}
-              | {:error, :invalid_job}
-
   @spec from_source(Source.t()) :: atom()
   def from_source(source) do
     Application.get_env(:cineplex, :reels)[source.reel]
